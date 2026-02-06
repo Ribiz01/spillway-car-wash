be5b23ed46ad38f85524fe808e60eb16000b290b
Ribiz01 <chrisribiz9@gmail.com>
2026-02-06 09:44:51 +0200

    Delete src directory

--- src/app/dashboard/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { DashboardLayout } from "@/components/dashboard-layout";
-
-export default function AttendantLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <DashboardLayout>{children}</DashboardLayout>;
-}
